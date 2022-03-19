b2b3190a9e7482564e5f1d2b031ecc00262af70b
david0617 <davidyu18800@gmail.com>
2022-03-18 21:26:49 -0700

    Win And Lose

---
+++ VRMobileProject/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int PlayerHP;
+    private Timer timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = gameObject.GetComponent<Timer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            PlayerHP--;
+            Destroy(other);
+        }
+        if(PlayerHP <= 0)
+        {
+            timer.endGame(false);
+        }
+    }
+}
